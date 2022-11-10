0b2d872b9826e00b20d6542fb26cd8632e08ec22
Elvin-Liu237 <elvinliu@live.unc.edu>
2022-11-10 12:56:45 -0500

    e

--- bin/cli.js
+++ bin/cli.js
@@ -3,11 +3,11 @@
 import { roll } from "/lib/roll.js"
 import minimist from "minimist"
 
-var argv = minimist(process.argv.slice(2))
-var sides = argv.sides || 6
-var dice = argv.dice || 2
-var rolls = argv.rolls || 1
-var results = roll(sides, dice, rolls)
+const args = minimist(process.argv.slice(2))
+const sidess = args.sides || 6
+const dices = args.dice || 2
+const rollss = args.rolls || 1
+const results = roll(sidess, dices, rollss)
 
 
 
