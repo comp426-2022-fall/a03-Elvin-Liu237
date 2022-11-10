6e34408e21bbd82365a7633d6abd4d92f0db73fe
Elvin-Liu237 <elvinliu@live.unc.edu>
2022-11-10 12:50:02 -0500

    new

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,15 @@
+#!/usr/bin/env node
+
 import { roll } from "/lib/roll.js"
+import minimist from "minimist"
+
+var argv = minimist(process.argv.slice(2))
+var sides = argv.sides || 6
+var dice = argv.dice || 2
+var rolls = argv.rolls || 1
+var results = roll(sides, dice, rolls)
+
+
+
+console.log(JSON.stringify(results))
 
