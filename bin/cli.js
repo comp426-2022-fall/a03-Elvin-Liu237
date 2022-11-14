b70c0de263c90c2bcecd04f2b7b75a4125946578
Elvin-Liu237 <elvinliu@live.unc.edu>
2022-11-14 11:32:11 -0500

    s

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
-import { roll } from "/lib/roll.js"
+import { roll } from "../lib/roll.js"
 import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2))
 const sidess = args.sides || 6
 const dices = args.dice || 2
 const rollss = args.rolls || 1
-const results = roll(sidess, dices, rollss)
 
-
-
-console.log(JSON.stringify(results))
+console.log(JSON.stringify(roll(sidess, dices, rollss)))
 
